f0ebfc252efee67c28207fc2a87fbe8a871daf9c
Muhammad Bakhtawar <m.bakhtawar.sce@gmail.com>
2024-03-11 12:53:30 +0500

    Completed 100%

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-// function evenorodd() {
-//     const a = prompt(`What do you want to Find? 1)Even 2)Odd`).toLowerCase();
-//     if (a === "1" || a === "even") {
-//         let limit = prompt(`How much even numbers do you want to print`);
-//         for (let i = 0; i <= limit; i += 2) {
-//             document.getElementById("paragraph").innerHTML += `<center><h3>${i}</h3></center>`;
-//         }
-//     } else if (a === "2" || a === "odd") {
-//         let limit = prompt(`How much odd numbers do you want to print`);
-//         for (let i = 1; i <= limit; i += 2) {
-//             document.getElementById("paragraph").innerHTML += `<center><h3>${i}</h3></center>`;
-//         }
-//     }
-// }
+function evenorodd() {
+    const a = prompt(`What do you want to Find? 1)Even 2)Odd`).toLowerCase();
+    if (a === "1" || a === "even") {
+        let limit = prompt(`How much even numbers do you want to print`);
+        for (let i = 0; i <= limit; i += 2) {
+            document.getElementById("paragraph").innerHTML += `<center><h3>${i}</h3></center>`;
+        }
+    } else if (a === "2" || a === "odd") {
+        let limit = prompt(`How much odd numbers do you want to print`);
+        for (let i = 1; i <= limit; i += 2) {
+            document.getElementById("paragraph").innerHTML += `<center><h3>${i}</h3></center>`;
+        }
+    }
+}
 
 function finiteorinfinite(){
   let a=prompt(`Enter the first number to divide`);
